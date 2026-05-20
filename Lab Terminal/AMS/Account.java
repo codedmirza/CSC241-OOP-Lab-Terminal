f6813174b89698017511abe8e9b07b9b4e35ea82
Muhammad Ashar Sheraz <freakingjerry499@gmail.com>
2026-05-20 14:07:07 +0500

    a commit message for the changes made to the Account.java file in the AMS package of the Lab Terminal project. The changes include adding an 'id' field to the Account class and initializing it in the constructor.

--- Lab Terminal/AMS/Account.java
+++ Lab Terminal/AMS/Account.java
@@ -4,7 +4,7 @@
 // Crew does NOT extend Account because employees don't log in to this system.
 
 public abstract class Account extends Person {
-
+    protected String id;       // Unique identifier for the account
     protected String username;
     protected String password;
     protected String email;
@@ -15,6 +15,7 @@ public abstract class Account extends Person {
     public Account(String id, String name, String phoneNumber, String address,
                    String username, String password, String email, String role) {
         super(id, name, phoneNumber, address);
+        this.id = id;
         this.username = username;
         this.password = password;
         this.email    = email;
