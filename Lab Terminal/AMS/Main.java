bb131ab685aa5422bbb5160fa7b471bba1097050
Zarak Khan <zak4473@gmail.com>
2026-05-24 23:10:34 +0500

    last test to check commit from vs code

--- Lab Terminal/AMS/Main.java
+++ Lab Terminal/AMS/Main.java
@@ -59,7 +59,10 @@ public class Main {
         a2.setAvailabilityStatus("Assigned");
         system.addFlight(f1);
         system.addFlight(f2);
+
 // zaki was here 2
+// one last test to check if commit is working fine
+
         User user = new User( "Faizan Ahmad", "0312-1112233", "Lahore",
                              "faizan", "1234", "faizan@mail.com",
                              "3520112345671", "Lahore");
