dd24f06af3b30ac393b174f669f5614c6512b8e2
Faizan Ahmad <faizanuni1st@gmail.com>
2026-05-28 12:31:29 +0500

    so i have simplified both Main.java and StartFrame.java. it still run fine. the looks are simple but we dont want it to be some fancy. Also i have removed that Lambda expression, and replace it with simple code. Lambda expressions are too complex. left the previous version as comment so if you guys don"t want this it can easily be undone. Also removed unncessory things like BorderFactory etc. or swing Utilities.

--- Lab Terminal/AMS/Main.java
+++ Lab Terminal/AMS/Main.java
@@ -1,7 +1,7 @@
 
 /*Functionality: created an airline object Loads saved data, 
 then opens first GUI StartFrame. File saving happens when window closes.*/ 
-import javax.swing.SwingUtilities;
+// import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 //changes: added comments, i removed demo data thing in main class.
@@ -9,19 +9,29 @@ import javax.swing.UIManager;
 public class Main {
 
     public static void main(String[] args) {
-               // System look and feel (Windows/Mac/Linux native look)
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ignored) {}
+        // // System look and feel (Windows/Mac/Linux native look)
+        // try {
+        //     UIManager.setLookAndFeel(
+        //             UIManager.getSystemLookAndFeelClassName());
+        // }
+        // catch (Exception e) {
+        //     System.out.println("Look and Feel not applied.");
+        // }
+
 
         // Create the main system object
         AirlineSystem system = new AirlineSystem();
 
         // Load previously saved data (users, flights, etc.)
         FileHandler.loadSystem(system);
-
+        // using SwingUtiklities is not necessory. 
         // Launch GUI on Event Dispatch Thread (Swing best practice)
-        SwingUtilities.invokeLater(() -> new StartFrame(system).setVisible(true));
+       //previously it was this [ SwingUtilities.invokeLater(() -> new StartFrame(system).setVisible(true));]
+       
+       //so made it simple,below 
+       // Open first window
+        StartFrame frame = new StartFrame(system);
+        frame.setVisible(true);
     }
 
 }
